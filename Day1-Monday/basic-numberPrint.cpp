f6fdd7b7fa2b619e51394a14f78eadc087beed76
suraj-kumar-sah <surajshah72600@gmail.com>
2026-01-19 22:27:40 +0530

    first code

---
+++ Day1-Monday/basic-numberPrint.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+  cout <<7 << endl;
+  cout << "7" << endl;
+  cout<< "7 + 4" << endl;
+  cout << 7 + 4 << endl;
+  return 0;
+}
---
+++ Day1-Monday/intro-variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int x; // initialization
+  x = 5; // declaration
+
+  int y = 9; // initialization + declaration (both)
+  cout << x;
+  cout << endl;
+  cout << y << endl;
+  return 0;
+}
